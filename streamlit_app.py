0cc49df42471de540bc881dd675d4692d91f2fb0
rbshirley <shirler@ccf.org>
2025-01-15 16:41:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,3 @@ if ingredients_list:
         # cnx.query(my_insert_stmt)
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
