8ba917d7f190dced9ae42eb088d9d6a259914c12
rbshirley <shirler@ccf.org>
2025-01-16 11:00:35 -0500

    Update streamlit_app.py
    add SEARCH_ON to query

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,10 @@ st.write(f'The name on your smoothie will be {name_on_order}')
 cnx = st.connection("snowflake")
 session = cnx.session() # session = cnx.session
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-# my_dataframe = cnx.query("SELECT FRUIT_NAME FROM FRUIT_OPTIONS")
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
+st.dataframe(data=my_dataframe,use_container_width = True)
+st.stop()
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
